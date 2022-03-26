e8e0d69bb45762c7e068323bbe07535a26b63ce5
ADNCRICH <p50629.2013x@gmail.com>
2022-03-27 00:25:19 +0700

    Prog

--- Programming/1010Word.cpp
+++ Programming/1010Word.cpp
@@ -1,23 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 char a[50][50], b[50];
-int n, m, ch, si, sj, len, di[8] = { 1, -1, 0, 0, 1, 1, -1, -1 }, dj[8] = { 0, 0, 1, -1, 1, -1, 1, -1 }, mk[50][50];
-void play(int i, int j, int s) {
+int n, m, ch, si, sj, len, di[8] = { -1,-1,0,1,1,1,0,-1 }, dj[8] = { 0,1,1,1,0,-1,-1,-1 };
+void play(int i, int j, int s, int k) {
     if (s == len - 1) {
         if (!ch)
             printf("%d %d\n", si, sj);
         ch = 1;
         return;
     }
-    for (int k = 0;k < 8;k++) {
-        int ii = i + di[k], jj = j + dj[k];
-        if (ii < 0 || jj < 0 || ii >= n || jj >= m || mk[ii][jj])    continue;
-        if (a[ii][jj] == b[s + 1]) {
-            mk[ii][jj] = 1;
-            play(ii, jj, s + 1);
-            mk[ii][jj] = 0;
-        }
+
+    int ii = i + di[k], jj = j + dj[k];
+    if (ii < 0 || jj < 0 || ii >= n || jj >= m)    return;
+    if (a[ii][jj] == b[s + 1]) {
+        play(ii, jj, s + 1, k);
     }
+
     return;
 }
 int main() {
@@ -34,14 +32,12 @@ int main() {
         for (i = 0;i < strlen(b); i++)b[i] = tolower(b[i]);
         len = strlen(b);
         ch = 0;
-        memset(mk, 0, sizeof mk);
         for (i = 0;i < n;i++)
             for (j = 0;j < m;j++) {
                 if (a[i][j] == b[0]) {
                     si = i, sj = j;
-                    mk[i][j] = 1;
-                    play(i, j, 0);
-                    mk[i][j] = 0;
+                    for (int k = 0;k < 8;k++)
+                        play(i, j, 0, k);
                 }
             }
     }
@@ -62,6 +58,6 @@ yUiqlxcnBje
 Compute
 Queue
 stack
-ieruraosucdq
+pointer
 
 */
--- Programming/tempCodeRunnerFile.cpp
+++ Programming/tempCodeRunnerFile.cpp
@@ -1,14 +1,1 @@
-8 11
-ascDEFGhigg
-hTqkComPutk
-FayUcompuTm
-FcsierMqsrc
-bkoArUePeyv
-Klcbqwekumk
-sreTNIophtb
-yUiqlxcnBje
-4
-Compute
-Queue
-stack
-ieruraosucdq
+
