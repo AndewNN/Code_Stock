e1b5b0acf95009d1e53ccd973a0c6d3bdf06af7a
ADNCRICH <p50629.2013x@gmail.com>
2022-03-29 22:30:18 +0700

    NICE

--- Programming/1024Factory.cpp
+++ Programming/1024Factory.cpp
@@ -3,12 +3,15 @@ algo : Combination
 */
 #include<bits/stdc++.h>
 using namespace std;
-int a[20], st = 2, n = 10, qs[1010], ans[2], now[5];
+int a[5], n, qs[1010], ans[2], now[5], mi = 1e9;
 void play(int l, int state) {
-    if (state == st) {
+    if (state == 2) {
         now[0] = qs[a[0]];
-        now[1] = qs[]
-            printf("\n");
+        now[1] = qs[a[1]] - qs[a[0]];
+        now[2] = qs[n] - qs[a[1]];
+        sort(now, now + 3);
+        if (now[2] - now[0] < mi)
+            mi = now[2] - now[0], ans[0] = a[0] + 1, ans[1] = a[1] + 1;
         return;
     }
     for (int i = l + 1;i < n;i++) {
@@ -18,12 +21,12 @@ void play(int l, int state) {
     return;
 }
 int main() {
-    int i, q;
-    scanf("%d", &q);
+    int i;
+    scanf("%d", &n);
     for (i = 1;i <= n;i++) {
         scanf("%d", &qs[i]);
         qs[i] += qs[i - 1];
     }
     play(0, 0);
-
+    printf("%d %d", ans[0], ans[1]);
 }
