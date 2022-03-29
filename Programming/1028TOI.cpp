83851391681b9e5f330937c7834254ae8340cc3c
ADNCRICH <p50629.2013x@gmail.com>
2022-03-30 00:41:05 +0700

    OKK

--- Programming/1028TOI.cpp
+++ Programming/1028TOI.cpp
@@ -1,10 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-int a[30];
+int a[30], n;
+void find(char opr, char r, char l) {
+    if (opr == '+')     a[r - 'A'] += a[l - 'A'];
+    if (opr == '-')     a[r - 'A'] -= a[l - 'A'];
+    if (opr == '*')     a[r - 'A'] *= a[l - 'A'];
+    if (opr == '/')     a[r - 'A'] /= a[l - 'A'];
+}
 int main() {
-    char o;
-    scanf(" %c", &o);
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    char o, c, d;
+    cin >> o;
     while (o != '!') {
-
+        cin >> c;
+        if (o == '=') {
+            cin >> n;
+            a[c - 'A'] = n;
+        }
+        else if (o == '#')
+            cout << a[c - 'A'] << "\n";
+        else {
+            cin >> d;
+            find(o, c, d);
+        }
+        cin >> o;
     }
+    cout << '!';
 }
