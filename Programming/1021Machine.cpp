12857d28ed8154edd02e079f57bb41df5795f155
ADNCRICH <p50629.2013x@gmail.com>
2022-03-29 21:52:12 +0700

    Prog

--- Programming/1021Machine.cpp
+++ Programming/1021Machine.cpp
@@ -1,5 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
+priority_queue<int> hp;
 int main() {
+    int n, i;
+    char o;
+    scanf("%d", &n);
+    while (n--) {
+        scanf(" %c", &o);
+        if (o == 'Q') {
+            if (!hp.empty()) {
+                printf("%d\n", hp.top());
+                hp.pop();
+            }
+            else
+                printf("-1\n");
+        }
+        else {
+            scanf("%d", &i);
+            hp.push(i);
+        }
+    }
 
 }
