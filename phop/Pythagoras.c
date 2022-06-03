b072eb3148ebea029bd9966402ceff462741254e
ADNCRICH <p50629.2013x@gmail.com>
2022-06-03 13:14:08 +0700

    new Folder

--- phop/Pythagoras.c
+++ phop/Pythagoras.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
-int main()
-{
+int main(){
     double a,b,c;
     scanf("%lf %lf %lf",&a,&b,&c);
     if (a==0)
@@ -9,4 +8,5 @@ int main()
         printf("%.2lf",pow((c*c-a*a),0.5));
     if (c==0)
         printf("%.2lf",pow((a*a+b*b),0.5));
-    }
+        
+}
