3c438ce9e476d73e5d4f7572f6137ea04ad657a9
ADNCRICH <p50629.2013x@gmail.com>
2023-02-17 11:22:13 +0700

    up

--- 00.cpp
+++ 00.cpp
@@ -1,32 +1,22 @@
 #include <math.h>
 #include <stdio.h>
 using namespace std;
-struct A {
-    int x, y, z;
-    double ans;
-};
-A aa[110];
-int x = -1, y, z, i, now = 0;
+int x = -1, y, z;
 double ans;
 bool input() {
     printf("Enter the X, Y, Z value (0 0 0 to exit): ");
     scanf("%d %d %d", &x, &y, &z);
-    aa[now].x = x, aa[now].y = y, aa[now].z = z;
     if (x == 0 && y == 0 && z == 0) return false;
     return true;
 }
 void output() {
     ans = sqrt(x * x + y * y + z * z);
-    aa[now++].ans = ans;
     printf("Lenght of vector (%d %d %d) is %.2lf\n", x, y, z, ans);
 }
 int main() {
     while (input()) {
         output();
     }
-    printf("--Summary--\n");
-    for (i = 0; i < now; i++)
-        printf("%d) Lenght of vector (%d %d %d) is %.2lf\n", i + 1, aa[i].x, aa[i].y, aa[i].z, aa[i].ans);
     printf("End of program. Goodbye.\n");
     return 0;
 }
--- KU01/2565/0.cpp
+++ KU01/2565/0.cpp
@@ -6,10 +6,17 @@
 */
 #include <bits/stdc++.h>
 using namespace std;
+int a[7] = {1, 5, 8, 10, 20, 25, 60};
+vector<int> aa;
 int main() {
-    char c;
-    cin >> c;
-    cout << c;
+    for (int i = 0; i < 7; i++)
+        aa.push_back(a[i]);
+    int c = 0;
+    while (c >= 0) {
+        cin >> c;
+        // cout << *upper_bound(aa.begin(), aa.end(), c) << "\n";
+        cout << *upper_bound(begin(a), end(a), c) << "\n";
+    }
 }
 /*
 14 13 17 15 15 14 15 28 18
